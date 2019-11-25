5ab0e2d30cea14f740d36ded406c279b433e2d35
kano233333 <1320395666@qq.com>
2019-11-25 17:17:42 +0800

    gameMain

--- assets/Script/common/c_toScene.js
+++ assets/Script/common/c_toScene.js
@@ -2,12 +2,12 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-       
+       scene: 'startGame'
     },
 
     start () {
         this.node.on('touchend', ()=>{
-            cc.director.loadScene('selectCheckpoint');
+            cc.director.loadScene(this.scene);
         })
     },
 
